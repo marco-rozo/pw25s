0ac727b6e0056aae837cbc03a2ac95b69f1faeff
marco-rozo <marcorozo99@gmail..com>
2022-09-08 21:57:26 -0300

    initial server commit

---
+++ server/server-project/src/test/java/br/edu/utfpr/pb/pw25s/serverproject/ServerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.pb.pw25s.serverproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
