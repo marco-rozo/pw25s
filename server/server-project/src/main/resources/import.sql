ebec7ca81882d8322cb02f28287bc6844e1f973e
marco-rozo <marcorozo99@gmail..com>
2022-10-06 18:57:28 -0300

    Comment sql script

--- server/server-project/src/main/resources/import.sql
+++ server/server-project/src/main/resources/import.sql
@@ -1,18 +1,18 @@
 -- INSERT INTO "category" (name) VALUES ( 'may@gmail.com', 'may', 'willem', '123');
-/*
-    private long id;
-
-    @NotNull
-    @OneToOne()
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @NotNull
-    private String name;
-
-    @NotNull
-    private String description;
-
-}
-
-*/
+-- /*
+--     private long id;
+--
+--     @NotNull
+--     @OneToOne()
+--     @JoinColumn(name = "user_id")
+--     private User user;
+--
+--     @NotNull
+--     private String name;
+--
+--     @NotNull
+--     private String description;
+--
+-- }
+--
+-- */
