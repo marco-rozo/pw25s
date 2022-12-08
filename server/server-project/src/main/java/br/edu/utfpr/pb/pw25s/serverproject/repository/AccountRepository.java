760e38a3184847e6dd3df38dc9f12b5b544db0be
marco-rozo <marcorozo99@gmail..com>
2022-12-08 00:36:26 -0300

    Retornando contas por usuário

--- server/server-project/src/main/java/br/edu/utfpr/pb/pw25s/serverproject/repository/AccountRepository.java
+++ server/server-project/src/main/java/br/edu/utfpr/pb/pw25s/serverproject/repository/AccountRepository.java
@@ -1,9 +1,13 @@
 package br.edu.utfpr.pb.pw25s.serverproject.repository;
 
 import br.edu.utfpr.pb.pw25s.serverproject.model.Account;
+import br.edu.utfpr.pb.pw25s.serverproject.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findAccountsByUser(User user);
 }
